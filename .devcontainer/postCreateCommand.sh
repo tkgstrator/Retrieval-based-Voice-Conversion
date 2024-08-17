c3bc93a349fe6091baa198f2f1a9c0de859d3a3f
tkgstrator <nasawake.am@gmail.com>
2024-08-18 06:26:18 +0900

    Use git-lfs

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,18 +1,37 @@
 #!/bin/zsh
 
-if [ ! -d "assets/rmvpe" ]; then
-  git clone https://huggingface.co/lj1995/VoiceConversionWebUI VoiceConversionWebUI
-  cd VoiceConversionWebUI
-  git checkout $SHA256_COMMIT_HASH
+function pull() {
+  local PATH="$1"
+  if [ ! "../assets/$PATH" ]; then
+    git lfs pull --include=$PATH
+    if [[ "$PATH" == *"rmvpe"* ]]; then
+      mv $PATH ../assets/rmvpe/$PATH
+    else
+      mv $PATH ../assets/$PATH
+    fi
+  fi
+}
+
+export GIT_CLONE_PROTECTION_ACTIVE=false
+export GIT_LFS_SKIP_SMUDGE=1
   
-  mkdir -p ../assets/rmvpe
-  mv pretrained ../assets/
-  mv uvr5_weights ../assets/
-  mv rmvpe.pt ../assets/rmvpe/rmvpe.pt
-  mv rmvpe.onnx ../assets/rmvpe/rmvpe.onnx
-  mv hubert_base.pt ../assets/hubert_base.pt
+mkdir -p assets/rmvpe
   
-  rm -rf ../VoiceConversionWebUI
-  poetry install --no-interaction --no-root
-  poetry cache clear --all .
-fi
+git clone --depth 1 https://huggingface.co/lj1995/VoiceConversionWebUI VoiceConversionWebUI
+cd VoiceConversionWebUI
+git config advice.detachedHead false
+git checkout $SHA256_COMMIT_HASH
+
+unset GIT_LFS_SKIP_SMUDGE
+unset GIT_CLONE_PROTECTION_ACTIVE
+
+pull "hubert_base.pt"
+pull "pretrained"
+pull "uvr5_weights"
+pull "rmvpe.pt"
+pull "rmvpe.onnx"
+
+cd ..
+rm -rf VoiceConversionWebUI
+poetry install --no-interaction --no-root
+poetry cache clear --all .
